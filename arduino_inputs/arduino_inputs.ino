8f15a10b04e56bd60d47e013cf853e42060e0da7
Kevin He <yc4he@uwaterloo.ca>
2018-10-23 01:51:07 -0400

    added Arduino DFU and base file

---
+++ arduino_inputs/arduino_inputs.ino
@@ -0,0 +1,30 @@
+const int buttonUp = 2;
+const int buttonDown = 3;
+const int buttonLeft = 4;
+const int buttonRight = 5;
+const int buttonPin = 6;
+const int ledPin = 13;
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
