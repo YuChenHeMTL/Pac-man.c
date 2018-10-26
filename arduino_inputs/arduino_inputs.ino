cedc8201d349df261ac8bd6fa1bf1246aad1e712
Kevin He <yc4he@uwaterloo.ca>
2018-10-25 23:15:45 -0400

    changed keyboard bindings

--- arduino_inputs/arduino_inputs.ino
+++ arduino_inputs/arduino_inputs.ino
@@ -1,7 +1,18 @@
-#define KEY_UP 38
-#define KEY_DOWN 40
-#define KEY_RIGHT 39
-#define KEY_LEFT 37
+//steps to DFU arduino
+//
+//Reset Arduino with onboard pins
+//Plug cycle the Arduino
+//Flash Arduino-usbserial.hex bootloader with dfu-programmer (erase/flash/reset)
+//Plug cycle the Arduino
+//Flash firmware sketch using Arduino IDE
+//Plug cycle the Arduino
+//Flash Arduino-keyboard-0.3.hex bootloader with dfu-programmer (erase/flash/reset)
+//Test and repeat
+
+#define KEY_W 26
+#define KEY_S 22
+#define KEY_D 7
+#define KEY_A 4
 
 #define PIN_UP 2
 #define PIN_DOWN 3
@@ -10,48 +21,49 @@
 
 uint8_t buf[8] = { 0 }; 
 
-int state = HIGH;
+int state = 1;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(PIN_UP, INPUT);
-  pinMode(PIN_DOWN, INPUT);
-  pinMode(PIN_RIGHT, INPUT);
-  pinMode(PIN_LEFT, INPUT);
+  pinMode(PIN_UP, INPUT_PULLUP);
+  pinMode(PIN_DOWN, INPUT_PULLUP);
+  pinMode(PIN_RIGHT, INPUT_PULLUP);
+  pinMode(PIN_LEFT, INPUT_PULLUP);
 
+  //internal pull up (set the state to 1)
   digitalWrite(PIN_UP, HIGH);
   digitalWrite(PIN_DOWN, HIGH);
   digitalWrite(PIN_RIGHT, HIGH);
   digitalWrite(PIN_LEFT, HIGH);
 
+  Serial.begin(9600);
   delay(200);
 }
 void loop() 
 {
   state = digitalRead(PIN_UP);
   if (state != 1) {
-    buf[2] = KEY_UP;    // Up key
+    buf[2] = KEY_W;    // Up key
     Serial.write(buf, 8); 
     releaseKey();
   } 
 
   state = digitalRead(PIN_DOWN);
   if (state != 1) {
-    buf[2] = KEY_DOWN;   // Down key
+    buf[2] = KEY_S;  //  Down key
     Serial.write(buf, 8); 
     releaseKey();
   } 
 
   state = digitalRead(PIN_RIGHT);
   if (state != 1) {
-    buf[2] = KEY_RIGHT;   // Right key
+    buf[2] = KEY_D; //   Right key
     Serial.write(buf, 8); 
     releaseKey();
   } 
   
   state = digitalRead(PIN_LEFT);
   if (state != 1) {
-    buf[2] = KEY_LEFT;   // Left key
+    buf[2] = KEY_A;  //  Left key
     Serial.write(buf, 8); 
     releaseKey();
   } 
@@ -61,5 +73,5 @@ void releaseKey()
 {
   buf[0] = 0;
   buf[2] = 0;
-  Serial.write(buf, 8); // Release key  
+  Serial.write(buf, 8);  //Release key  
 }
